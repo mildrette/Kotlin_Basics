3456c30c666ea450bdedaeec0faab61d3ab125da
mildrette <mildredfonka1@maild.com>
2026-02-17 17:55:58 +0100

    starting afresh with kotlin basic

---
+++ src/lesson.kt
@@ -0,0 +1,25 @@
+fun main(){
+    println("Hello World")
+
+    val name = "Mildred"
+    var age:Int = 30
+    println("my name is $name and i am $age years old")
+
+
+    var number  = 30
+    number += 31
+    number -= 30
+    number *= 30
+    println("my age is $number and i am $age years old")
+
+    var learning: Int
+    learning = 1
+
+    var kotlin:String
+    kotlin = "Day 1"
+    println(learning)
+    println(kotlin)
+
+
+//    read only list
+}
