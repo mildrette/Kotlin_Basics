959881352197bd39d1af7237ae020354009a7838
Mildrette <mildredfonka1@gmail.com>
2026-07-31 22:39:48 +0100

    retaking lessons on the basics of kotlin

---
+++ hello.kt
@@ -0,0 +1,22 @@
+fun main(){
+val name = "Mildred"
+var age = 31
+var isLearning = true
+var city = "Bamenda"
+var fovoriteColor = "violet"
+
+println("Hello everyoone, my naem is $name, I am $age years old and it is $isLearning that I am learning Kotlin to day, wish me luck here ")
+println("I am presently living in the city of $city and i love the color $fovoriteColor")
+
+
+
+var height = 1.6
+height = 1.7
+
+println(height)
+
+var weight = 72
+weight = 60
+
+println(weight)
+}
--- src/Day2.kt
+++
@@ -1,2 +0,0 @@
-//Goal: Make programs interactive.
-
