a97e646b61f26d0d6da6fbb68a964d6d79344cd2
Mildrette <mildredfonka1@gmail.com>
2026-08-09 17:02:01 +0100

    Build mini message length checker using functions, loops, and return values

--- hello.kt
+++ hello.kt
@@ -1,89 +1,18 @@
 fun main(){
-// val name = "Mildred"
-// var age = 31
-// var isLearning = true
-// var city = "Bamenda"
-// var fovoriteColor = "violet"
 
-// println("Hello everyoone, my naem is $name, I am $age years old and it is $isLearning that I am learning Kotlin to day, wish me luck here ")
-// println("I am presently living in the city of $city and i love the color $fovoriteColor")
+var messageLengths = arrayOf(50, 100, 150, 200, 250)
 
-
-
-// var height = 1.6
-// height = 1.7
-
-// println(height)
-
-// var weight = 72
-// weight = 60
-
-// println(weight)
-
-
-// conditionals statements
-    // var sweets = 20
-
-    // if(sweets == 20){
-    //     println("I have $sweets sweets")
-    // }
-    // else if(sweets != 20){
-    //     println("I do not have $sweets sweets")
-    // }
-    // else{
-    //     println("I have no sweets")}
-
-
-// loops 
-
-        // for (i in 1..3){
-        //     println("I made it to step $i")
-        // }
-
-
-        // var meals = 10
-        // while (meals > 0){
-        //     println("slurp, soup left $meals")
-        //     meals = meals - 1
-        // }
-
-        // for (greetings in 1..5){
-        //     println ("Hello, Good morning Number $greetings")
-        // }
-
-
-        // var names = arrayOf("Mildred", "Moses", "Moses", "Moses", "Moses")
-
-        // for (name in names){
-        //     println("Hello $name, welcome to my channel")
-        // }
-
-        // var favoriteFruits = arrayOf("Nango", "Banana", "Avocado", "Pineapple", "Strawberry")
-
-        // for (fruit in favoriteFruits){
-        //     println("I love eating $fruit")
-        // }
-
-
-        // functions
-// learning("Kotlin")
-// learning("Java")
-welcomeDesk("Mildred", 31, "Singing")
-    
+for(i in messageLengths){
+    var canSend = canSendMessages(i)
+    println("$i characters : can send? $canSend")
 }
 
-
-    // fun learning(lesson: String){
-    //         println("I am Learning $lesson")
-    //     }
+ }
 
 
 
-
-        fun welcomeDesk(name: String, age: Int, hobby: String){
-            for(i in 1..3){
-                println("i love $hobby")
-            }
-
-            println("Hello everyone, my name is $name, I am $age years old and I love $hobby")
-        }
+fun canSendMessages(messages: Int): Boolean{
+    if(messages <= 160){
+        return true
+    } else { return false}
+}
---
+++ test.kt
@@ -0,0 +1,132 @@
+// fun main(){
+// // val name = "Mildred"
+// // var age = 31
+// // var isLearning = true
+// // var city = "Bamenda"
+// // var fovoriteColor = "violet"
+
+// // println("Hello everyoone, my naem is $name, I am $age years old and it is $isLearning that I am learning Kotlin to day, wish me luck here ")
+// // println("I am presently living in the city of $city and i love the color $fovoriteColor")
+
+
+
+// // var height = 1.6
+// // height = 1.7
+
+// // println(height)
+
+// // var weight = 72
+// // weight = 60
+
+// // println(weight)
+
+
+// // conditionals statements
+//     // var sweets = 20
+
+//     // if(sweets == 20){
+//     //     println("I have $sweets sweets")
+//     // }
+//     // else if(sweets != 20){
+//     //     println("I do not have $sweets sweets")
+//     // }
+//     // else{
+//     //     println("I have no sweets")}
+
+
+// // loops 
+
+//         // for (i in 1..3){
+//         //     println("I made it to step $i")
+//         // }
+
+
+//         // var meals = 10
+//         // while (meals > 0){
+//         //     println("slurp, soup left $meals")
+//         //     meals = meals - 1
+//         // }
+
+//         // for (greetings in 1..5){
+//         //     println ("Hello, Good morning Number $greetings")
+//         // }
+
+
+//         // var names = arrayOf("Mildred", "Moses", "Moses", "Moses", "Moses")
+
+//         // for (name in names){
+//         //     println("Hello $name, welcome to my channel")
+//         // }
+
+//         // var favoriteFruits = arrayOf("Nango", "Banana", "Avocado", "Pineapple", "Strawberry")
+
+//         // for (fruit in favoriteFruits){
+//         //     println("I love eating $fruit")
+//         // }
+
+
+//         // functions
+// // learning("Kotlin")
+// // learning("Java")
+// // welcomeDesk("Mildred", 31, "Singing")
+
+// // var greeting = makeHi()
+// // println(greeting)
+   
+// var sleep = canSleep(7)
+// println(sleep)
+//  }
+
+//     fun canSleep(hours: Int): Boolean{
+//             if(hours >= 8){
+//                 return true
+//             }
+//             else
+//             {return false}
+//         }
+
+//     // fun learning(lesson: String){
+//     //         println("I am Learning $lesson")
+//     //     }
+
+
+//         // fun welcomeDesk(name: String, age: Int, hobby: String){
+//         //     for(i in 1..3){
+//         //         println("i love $hobby")
+//         //     }
+
+//         //     println("Hello everyone, my name is $name, I am $age years old and I love $hobby")
+//         // }
+
+//         // fun makeHi(): String{
+//         //     return "Hi"
+//         // }
+
+// fun main(){
+// var sleep = canSleep(7)
+// println(sleep)
+
+//  }
+
+//     fun canSleep(hours: Int): Boolean{
+//             if(hours >= 8){
+//                 return true
+//             }
+//             else
+//             {return false}
+//         }
+
+
+//      fun main(){
+
+// var messageLength = canSendMessages(158)
+// println("$messageLength is the right characters need")
+//  }
+
+  
+
+// fun canSendMessages(messages: Int): Boolean{
+//     if(messages <= 160){
+//         return true
+//     } else { return false}
+// }
